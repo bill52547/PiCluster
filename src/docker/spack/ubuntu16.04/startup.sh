999ca41be051aeec0cccd9b99ee16a7c57f72061
HongXiang <hx.hongxiang@gmail.com>
2018-07-20 19:45:15 +0800

    spack debugging

--- src/docker/spack/ubuntu16.04/startup.sh
+++ src/docker/spack/ubuntu16.04/startup.sh
@@ -2,4 +2,4 @@
 echo Runing startup script...
 export SPACK_ROOT=/opt/spack/spack
 export PATH=$SPACK_ROOT/bin:${PATH}
-. $SPACK_ROOT/share/spack/setup-env.sh
+#. $SPACK_ROOT/share/spack/setup-env.sh
