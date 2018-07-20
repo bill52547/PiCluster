aa2f3f13ae6d83da7ef8272ce5a30fe214b252f1
HongXiang <hx.hongxiang@gmail.com>
2018-07-20 13:59:49 +0800

    slurm-ctrl worked

--- src/docker/slurm/control/startup.sh
+++ src/docker/slurm/control/startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo Runing startup script...
-. $SPACK_ROOT/share/spack/setup-env.sh
+#. $SPACK_ROOT/share/spack/setup-env.sh
+/etc/init.d/munge restart
+/etc/init.d/slurmctld restart
 
 #spack load slurm munge htop
 #export slurmctld=`spack location --install-dir slurm`/sbin/slurmctld 
