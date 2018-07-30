1a5d0fdb7b00d7324b517239788ecf17693a80db
twj2417 <twj2417@163.com>
2018-07-30 14:12:34 +0800

    delete

--- src/python/dxl/cluster/taskguard/cycle.py
+++ src/python/dxl/cluster/taskguard/cycle.py
@@ -58,37 +58,6 @@ def graph_cycle():
     g = Graph(get_nodes(),get_depens())
     g.mark_complete()
     runable_tasks = g.all_runable()
-    # tasks_to_submit = (web.Request().read_all()
-    #           .filter(lambda t:t.id in runable_tasks)
-    #           .filter(lambda t:t.is_before_submit))
-    # tasks_withoutgpu = (tasks_to_submit.filter(lambda t:t.is_depen_gpu==False)
-    #           .to_list()
-    #           .subscribe_on(rx.concurrency.ThreadPoolScheduler())
-    #           .to_blocking().first())
-    # for i in tasks_withoutgpu:
-    #     if i.worker==base.Worker.Slurm:
-    #         task_submit = slurm.Slurm().submit(i)
-    #     else:
-    #         task_submit = i.update_state(base.State.Runing)
-    #         # task_submit = task_submit.update_start()
-    #         web.Request().update(task_submit)
-    # tasks_withgpu = (tasks_to_submit.filter(lambda t:t.is_depen_gpu)
-    #           .to_list()
-    #           .subscribe_on(rx.concurrency.ThreadPoolScheduler())
-    #           .to_blocking().first())
-    # for i in tasks_withgpu:
-    #     ni= allocate_node(i)
-    #     if ni is not None:
-    #         if ni.worker==base.Worker.Slurm:
-    #             task_submit = slurm.Slurm().submit(i)
-    #         elif ni.worker==base.Worker.NoAction:
-    #             task_submit = ni.update_state(base.State.Runing)
-    #         # task_submit = task_submit.update_start()
-    #             web.Request().update(task_submit)          
-    # # tasks_to_submit = (web.Request().read_all().filter(lambda t:t.id in runable_tasks)
-    # #           .filter(lambda t:t.is_before_submit))
-    # # slurm_task = (tasks_to_submit.filter(lambda t:t.worker==base.Worker.Slurm)
-    # #               .map(lambda t:slurm.Slurm().submit(t)).map(lambda t:slurm.Slurm().update(t)))
     tasks_to_submit = (web.Request().read_all()
               .filter(lambda t:t.id in runable_tasks)
               .filter(lambda t:t.is_before_submit)
