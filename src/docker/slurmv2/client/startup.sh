5bfcc801db7ec7ee9b9760e5daa448d947878f88
tsinglung <tsinglung.tseng@gmail.com>
2018-10-12 10:13:19 +0800

    new slurm client, purify env

--- src/docker/slurmv2/client/startup.sh
+++ src/docker/slurmv2/client/startup.sh
@@ -1,11 +1,3 @@
-# /bin/sh
+#!/bin/bash
 echo Runing startup script...
-#. $SPACK_ROOT/share/spack/setup-env.sh
 /etc/init.d/munge restart
-#echo Spin up Gate7.2...
-#./opt/spack/share/spack/setup-env.sh
-#source <(spack module loads --dependencies gate@7.2)
-#source geant4.sh
-#spack load slurm munge htop
-#export slurmctld=`spack location --install-dir slurm`/sbin/slurmctld 
-#export munged=`spack location --install-dir slurm`/sbin/munged 
