f9c0fb24b42ad9a78e0d65f2aa37a67407d4acb7
HongXiang <hx.hongxiang@gmail.com>
2018-10-15 16:38:04 +0800

    change depens  for CI

--- tests/python/cluster/database2/test_crud.py
+++ tests/python/cluster/database2/test_crud.py
@@ -1,7 +1,5 @@
 from dxl.cluster.database2 import Task, TaskState
 import arrow
-import pytest
 
-@pytest.mark.skip("Can not find bug")
 def test_create():
-    t = Task(state=TaskState.Unknown, scheduler="slurm.sjtu.cluster.pitech.com", create=arrow.now(), depens=[3])
+    t = Task(state=TaskState.Unknown, scheduler="slurm.sjtu.cluster.pitech.com", create=arrow.now(), depens=[4])
